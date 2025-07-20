5d34cd4a7889335ae0283f65537c1d170726440c
IbragimKhaniev <newinterny@gmail.com>
2025-07-21 02:28:24 +0300

    great

--- server/db.js
+++ server/db.js
@@ -1,3 +1,6 @@
+/** Не редактируй вообще код этого файла, импортируй в необходимые файлы mongoDb и используй его. */
+const mongoose = require('mongoose');
+
 const MONGO_URI = process.env.MONGO_URI;
 
 const mongoDb = mongoose.createConnection(MONGO_URI);
